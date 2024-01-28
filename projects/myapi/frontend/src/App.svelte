6f0884de27a2591586fe54bbf0a3ccbaf50466d8
JaeDoo1034 <48791779+JaeDoo1034@users.noreply.github.com>
2024-01-28 22:44:05 +0900

    app.svelte 변경
    비동기 방식으로 변수 연계
     - await 구문.
     - promise

--- projects/myapi/frontend/src/App.svelte
+++ projects/myapi/frontend/src/App.svelte
@@ -8,8 +8,8 @@
   });
 </script> -->
 
-<!-- 비동기 방식 -->
-<script>
+<!-- 비동기 방식  : chatgpt-->
+<!-- <script>
   let message;
 
   async function fetchMessage() {
@@ -19,6 +19,25 @@
   }
 
   fetchMessage();
+</script> -->
+
+<script>
+  async function hello() {
+    const res = await fetch("http://127.0.0.1:8000/hello");
+    const json = await res.json();
+
+    if (res.ok) {
+      return json.message;
+    } else {
+      alert("error");
+    }
+  }
+
+  let promise = hello();
 </script>
 
-<h1>{message}</h1>
+{#await promise}
+  <p>...waiting</p>
+{:then message}
+  <h1>{message}</h1>
+{/await}
