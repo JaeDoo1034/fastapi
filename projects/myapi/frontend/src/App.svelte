fea9d3aef3efcafad2c39e33b251aaeecaaa361c
JaeDoo1034 <48791779+JaeDoo1034@users.noreply.github.com>
2024-01-28 22:40:44 +0900

    fastap vs svelte
    fastapi docs swagge에서 try out 켜놓으면 서버통신이 안됨.

--- projects/myapi/frontend/src/App.svelte
+++ projects/myapi/frontend/src/App.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script> 동기 방식
   let message;
 
   fetch("http://127.0.0.1:8000/hello").then((response) => {
@@ -6,6 +6,19 @@
       message = json.message;
     });
   });
+</script> -->
+
+<!-- 비동기 방식 -->
+<script>
+  let message;
+
+  async function fetchMessage() {
+    const response = await fetch("http://127.0.0.1:8000/hello");
+    const json = await response.json();
+    message = json.message;
+  }
+
+  fetchMessage();
 </script>
 
-<h1>hello</h1>
+<h1>{message}</h1>
--- projects/myapi/main.py
+++ projects/myapi/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware # FASTAPI에 CORS 예외 URL 등록.
+
 app = FastAPI()
 
-@app.get('/hello')
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+@app.get("/hello")
 def hello():
-    return {"message" : "안녕하세요!"}
+    return {"message" : "Hello"}
