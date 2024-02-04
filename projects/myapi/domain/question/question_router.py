08d395e42947d162d781f94d88aec27f37f60a15
JaeDoo1034 <48791779+JaeDoo1034@users.noreply.github.com>
2024-02-04 22:16:09 +0900

    의존성_contextmanager
    contextmanager 사용

--- projects/myapi/domain/question/question_router.py
+++ projects/myapi/domain/question/question_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from database import SessionLocal
+from database import get_db
 from models import Question
 
 #  --- 라우터 파일에 반드시 필요한 것은 APIRouter클래스로 생성한 router객체.
@@ -17,7 +17,12 @@ router = APIRouter(
 # db 세션 객체를 생성한 후에 db.close()를 수행하지 않으면 SQLAlchemy가 사숑하는 커넥션 풀에 db세션이 반환되지 않는다.
 @router.get("/list")
 def question_list():
-    db = SessionLocal()
-    _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
-    db.close() # 사용한 세션을 커넥션 풀에 반환하는 함수. (세션 종료가 아니다!)
+    
+    # contextlib의 contextmanager 사용
+    with get_db() as db:
+      _question_list = db.query(Question).order_by(Question.create_date.desc()).all()  
+    
+    # db = SessionLocal()
+    # _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+    # db.close() # 사용한 세션을 커넥션 풀에 반환하는 함수. (세션 종료가 아니다!)
     return _question_list
